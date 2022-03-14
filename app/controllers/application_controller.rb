450fed25c871994395d57d8a115ab36e4ea44d42
shu Lin <bbqlinshu@gmail.com>
2022-03-13 23:45:01 -0400

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,34 @@ class ApplicationController < Sinatra::Base
     game = Game.find(params[:id])
 
     game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
+      reviews: { only: [:id, :comment, :score], include: {
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    ).to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
